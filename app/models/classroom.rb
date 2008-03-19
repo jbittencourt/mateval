2ec483a89484c74dfe9b61b73e99379cb4e8c0f0
Felipe Tanus <fotanus@gmail.com>
2008-03-19 21:19:27 +0000

    list_students method added

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -1,18 +1,25 @@
 class Classroom < ActiveRecord::Base
-  has_and_belongs_to_many :diario_de_classe_posts
-  has_and_belongs_to_many :students
 
-def list_students
-	#find all students id
-	students_ids = StudentClassroom.find(:all, :conditions => "classroom_id = " self.id.to_s )
+	has_and_belongs_to_many :diario_de_classe_posts
+	has_and_belongs_to_many :students
 
-	#for each id list the username
-	students_ids.each do |s_id|
-		student = Student.find(:all, :conditions => "id =" s_id.id.to_s)
-		student.each do |stu|
-			y stu.name
-		end
+
+	def list_students(year=Date.today.to_s[0,4])
+		#pesquisar por ano e depois pedir os alunos
+
+		classroom = Classroom.find(:first, :conditions => "year='#{year}' AND name='#{self.name}'" )		
+		return classroom.students
 	end
-end
 
+	def self.list_invalid_classrooms 
+
+		today = Date.today.to_s
+		current_year = today[0,4]
+		return Classroom.find(:all, :conditions => "year <= " << current_year)
+		#valid_class.each do |vc|
+		#	y vc.name
+		#end
+	end
+
+	
 end
