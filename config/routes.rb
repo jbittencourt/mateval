0ceb31dd4f026f2bd744665afc2e7ebcf4e5c089
Soraya Sybele Hossain <sshossain@gmail.com>
2008-05-13 18:01:26 +0000

    Login and Register routes added

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ ActionController::Routing::Routes.draw do |map|
 
   map.resources :comments
 
+  map.resources :login
+
+  map.resources :register
+
   map.resources :diario_de_classe_posts, :has_many => :comments
 
   # The priority is based upon order of creation: first created -> highest priority.
