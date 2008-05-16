6b64cd8ef1887397d445c5f18a524532b66c10c2
Felipe Tanus <fotanus@gmail.com>
2008-05-16 20:38:51 +0000

    routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,9 @@ ActionController::Routing::Routes.draw do |map|
 
   map.resources :diario_de_classe_posts, :has_many => :comments
 
+  map.resources :account
   # The priority is based upon order of creation: first created -> highest priority.
-  map.root :controller => 'account'
+  #map.root :controller => 'account'
   
 
 
