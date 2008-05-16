d839fab5b9d0b54fc88009a721522ae5ae803a95
Felipe Tanus <fotanus@gmail.com>
2008-05-16 20:37:34 +0000

    application_helper updated;

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,24 +1,40 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
 
+
 	def left_box
-		return " <img src='images/unlogged/login.jpg'>
-					<div class=\"login\">
-						</br>
-						Professor(a):</br>
-						<input type=\"text\"></br></br>
-						Senha:</br>
-						<input type=\"password\">
-					</div>
-					<div class=\"minor-box\">
-						</br>Esqueceu a senha?
-					</div>"
+		if logged_in?
+			"loged"
+		
+		else
+			output = []
+			output << form_tag('/account/login')
+			output << image_tag("/images/unlogged/login.jpg") 
+			output << "
+					<div class=\"login\"><br/>
+					Professor(a):</br>
+					<input type=\"text\"><br/><br/>
+					Senha:</br>
+					<input type=\"password\">"
+					output << submit_tag("login")
+					output << "</form>"
+					output << "</div>
+				<div class=\"minor-box\">
+					</br>Esqueceu a senha?
+				</div>"
+
+			return output.join("\n ")
+		end
 	end
 
 	def top
-		return "<div class=\"banner\">
+		if logged_in?
+			"loged"
+		else
+		 "<div class=\"banner\">
 				<img src=\"images/unlogged/top.jpg\" alt=\"Escola Luciana de Abreu\">
 			</div>"
+		end
 	end
 
 end
