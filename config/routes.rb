ff86c3b532c8dc830569935cd6dd28171c5b4990
Robson Mendonça <mwoc@gmail.com>
2008-06-03 00:35:29 +0000

    Update routes for default entrance of tool, and some corrections anotated into the code.

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,38 @@
 ActionController::Routing::Routes.draw do |map|
-  map.connect ':controller/:action/:id'
-  map.resources :classrooms
-
-  map.resources :subjects
-
-  map.resources :comments
-
-  map.resources :login
-
-  map.resources :register
-
-  map.resources :diario_de_classe_posts, :has_many => :comments
-
-  map.resources :account
-
-  
-
-
   # The priority is based upon order of creation: first created -> highest priority.
-  #map.root :controller => 'account'
+  map.resources :classrooms
   
-
-
-
+  map.resources :subjects
+  
+  map.resources :comments
+   
+  map.resources :register
+  
+  map.resources :diario_de_classe_posts, :has_many => :comments
+  
+  # Account mapping
+  #
+  # This rules are responsible for make connection with account actions 
+  # for login, signup and logout
+  #
+  map.resources :account
+  
+  map.login 'login', :controller => 'account', :action => 'login'
+  map.logout 'logout', :controller => 'account', :action => 'logout'
+  map.signup 'signup', :controller => 'account', :action => 'signup'
+  
+  # Sample of regular route:
+  # map.connect 'products/:id', :controller => 'catalog', :action => 'view'
+  # Keep in mind you can assign values other than :controller and :action
+  
+  # Sample of named route:
+  # map.purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
+  # This route can be invoked with purchase_url(:id => product.id)
+  
+  # Putting the account controller as root controller 
+  map.connect '', :controller => "account"
+  
+  # Install the default route as the lowest priority.
+  map.connect ':controller/:action/:id.:format'
+  map.connect ':controller/:action/:id'
 end
