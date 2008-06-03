af9eaff16d9f49eb8da5e275676f34af5f90ee9a
Robson Mendonça <mwoc@gmail.com>
2008-06-03 00:36:32 +0000

    Fix the login process

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -3,12 +3,13 @@ class AccountController < ApplicationController
   include AuthenticatedSystem
   # If you want "remember me" functionality, add this before_filter to Application Controller
   before_filter :login_from_cookie
-
+  
   # say something nice, you goof!  something sweet.
   def index
+    redirect_to :controller => 'classrooms' if logged_in?
     redirect_to(:controller => '/login') unless logged_in? || User.count > 0
   end
-
+  
   def login
     return unless request.post?
     self.current_user = User.authenticate(params[:username], params[:password])
@@ -17,33 +18,28 @@ class AccountController < ApplicationController
         # a trick to get the user stored in the session
         User.current_user = self.current_user
         #-----------------------------------------------
-			
+        
         self.current_user.remember_me
         cookies[:auth_token] = { :value => self.current_user.remember_token , :expires => self.current_user.remember_token_expires_at }
       end
-
-		#geting session variables
-		session[:name] = self.current_user.name
-		session[:user_id] = self.current_user.id
-
-      redirect_back_or_default(:controller => '/login', :action => 'index')
-      flash[:notice] = "Logged in successfully"
-    else
-		flash[:notice] = "Wrong username or password"
-		redirect_to(:controller => '/login')
-
-	 end
-
+      
+      #geting session variab
+      session[:name] = self.current_user.name
+      session[:user_id] = self.current_user.id
+      
+      redirect_back_or_default(:controller => '/classrooms', :action => 'index')
+      #flash[:notice] = ""
+    end
   end
-
+  
   def signup
     @user = User.new(params[:user])
     return unless request.post?
     @user.save!
     self.current_user = @user
-    redirect_back_or_default(:controller => '/login', :action => 'index')
+    redirect_back_or_default(:controller => '/account', :action => 'index')
     flash[:notice] = "Thanks for signing up!"
- 	 rescue ActiveRecord::RecordInvalid
+  rescue ActiveRecord::RecordInvalid
     render :action => 'signup'
   end
   
@@ -52,6 +48,6 @@ class AccountController < ApplicationController
     cookies.delete :auth_token
     reset_session
     flash[:notice] = "You have been logged out."
-    redirect_back_or_default(:controller => '/login', :action => 'index')
+    redirect_back_or_default(:controller => '/account', :action => 'index')
   end
 end
