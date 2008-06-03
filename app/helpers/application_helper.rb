1f1573d1a078a14f15c963922c95aa4a91cf646a
Robson Mendonça <mwoc@gmail.com>
2008-06-03 00:41:54 +0000

    Become more clear the helpers used to render the menus and sidebar.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,42 +2,29 @@
 module ApplicationHelper
 
 
-	def left_box
+  # Render the login box or profile side_bar
+  #
+  # The html files leave in:
+  #   * app/views/account/_login_box
+  #   * app/view/account/_profile_box
+  #
+  # TASK: in the future make this work with administrator and
+  # teacher differentiation
+  #
+	def side_bar_left
 		if logged_in?
-			output = []
-			output << image_tag("/images/logged/dummy.jpg") 
-			output <<	"<div class=\"user-menu\">"
-			output <<     "<b>" + session[:name] + "</b><br/>"
-			output <<		"Editar Perfil<br />"
-			output <<		button_to ("Sair" , :controller => :account , :action => :logout)
-			output <<	"</div>"
-
-			output << "<div class=\"orange-box\"></div>"
-			output << image_tag("/images/logged/pcn.jpg", :style => "margin-bottom: 0px")
-			return output.join("\n ")
-
-		else
-			output = []
-			output << form_tag('/account/login')
-			output << image_tag("/images/unlogged/login.jpg") 
-			output << "
-						<div class=\"login\"><br/>
-						Professor(a):</br>
-						<input type=\"text\" name=\"username\"><br/><br/>
-						Senha:</br>
-						<input type=\"password\" name=\"password\">"
-			output << submit_tag("login")
-			output << "</form>"
-			output << "</div>
-				<div class=\"minor-box\">
-					</br>Esqueceu a senha?
-				</div>"
-
-			return output.join("\n ")
+      return render :partial => 'account/profile_box'  
+    else
+      return render :partial => 'account/login_box'  
 		end
 	end
 
-	def top
+  # Render the main menu, located in the top of site
+  #
+  # TASK: in the future make this work with administrator and
+  # teacher differentiation
+  #
+	def header_menu
 		if logged_in?
 			output = []
 			output << image_tag("/images/logged/my_classes.jpg") 
