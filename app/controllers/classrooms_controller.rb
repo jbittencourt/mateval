351c33e9d4f59d6bc5940fc26c5b3b468240b5b0
Felipe Tanus <fotanus@gmail.com>
2008-06-05 18:03:35 +0000

    select on first logged layout

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -1,6 +1,16 @@
 class ClassroomsController < ResourceController::Base
 	
 	def index
+		#get all the classrooms relationated whit the user
 		@classrooms = current_user.classrooms.find(:all)
+
+		#get distinct years from his classrooms
+		@distinct_years = current_user.classrooms.find(:all).map{ |i| i.year }.uniq
+#		@distinct_years = []
+#			@classrooms.each do |c|
+#				if !@distinct_years.include?(c.year) 
+#					@distinct_years.push(c)
+#				end
+#			end
 	end
 end
