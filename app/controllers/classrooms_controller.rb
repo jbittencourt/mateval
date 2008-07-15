50d33bd733349fb010d08a8de0c00b0ca011dc67
Robson Mendonça <mwoc@gmail.com>
2008-07-15 19:39:13 +0000

    Commit do diario de classe

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -18,29 +18,7 @@ class ClassroomsController < ResourceController::Base
       :actions => %w(objectives),
       :action_buttons => ['select_year(@distinct_years,nil)']
     }
- 
-  
-  
-    #get all the classrooms relationated whit the user
-#    classrooms = current_user.classrooms.find(:all)
-    
-    #get distinct years from his classrooms
-#    @distinct_years = current_user.classrooms.find(:all).map{ |i| i.year }.uniq
-    
-#    @datagrid = { 
-#      :title => 'Turmas', 
-#      :list => classrooms,
-#      :header => %w(Nome),
-#      :fields => %w(name),
-#      :actions => %w(objectives),
-#      :action_buttons => ['select_year(@distinct_years,nil)']
-#    }
-    #		@distinct_years = []
-    #			@classrooms.each do |c|
-    #				if !@distinct_years.include?(c.year) 
-    #					@distinct_years.push(c)
-    #				end
-    #			end
+
   end
   
   def objectives
@@ -75,18 +53,6 @@ class ClassroomsController < ResourceController::Base
     
   end
   
-  #  def create
-  #    if !params[:name].nil?
-  #      classroom = Classroom.new
-  #      classroom.name = params[:name]
-  #      classroom.year = params[:year] #Date.today.to_s[0,4]
-  #      classroom.save
-  #      redirect_to :action => 'manager'
-  #    else
-  #      redirect_to :action => 'new'
-  #    end
-  #  end
-  
   def create
     classroom = Classroom.new
     classroom.name = params[:classroom][:name]
@@ -115,6 +81,11 @@ class ClassroomsController < ResourceController::Base
   end
   
   def new
+    render :layout => false
+  end
+  
+  def list_students
+    @classroom = Classroom.find(params[:id])
     render :layout => false
   end
   
